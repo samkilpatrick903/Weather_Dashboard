57295f858616c9aed0d830d35de774af8e860a1d
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-10-24 18:19:52 -0500

    added event listener

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,21 @@ var currentWeather = $("currentWeather");
 var forecastContainer = $(".card-deck");
 var search = $(".btn");
 var searchHistory = [];
+
+search.on("click", function(event) {
+    event.preventDefault();
+    var cityName = $(".inputArea").val();
+    console.log(cityName);
+
+    var searchedBtn = $("<button class='btn btn-primary' type='button'>Search</button>");
+    searchedBtn.click(function(event){
+            event.preventDefault();
+    })
+
+    searchHistory.push(cityName);
+    console.log(cityName);
+    const jsonCityArr = JSON.stringify(searchHistory);
+    localStorage.setItem("city", jsonCityArr);
+    searchedBtn.text(cityName);
+    getWeather(cityName);
+});
